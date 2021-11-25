7552cbd4e49d0625bfdfccd36e17f8870894803a
Fabiany Lopes <fabiany.lopes.060@ufrn.edu.br>
2021-11-25 14:46:06 -0300

    js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 let cont = 0;
 
+let modelo;
+let gola;
+let tecido;
+
+let urlImagem = "https://www.industriallogic.com/img/blog/2013/09/800px-Slava_Zaitsev_fashion_show-2.jpg";
+
 //let nome = prompt("Qual o seu lindo nome? ");
 
 function selecionarModelo(item) {
@@ -12,7 +18,10 @@ function selecionarModelo(item) {
         cont++;
     }
 
+    modelo = "blusa"
+
     marcado.classList.add("selecionado");
+    verificarPedido();
 }
 
 function selecionarGola(item) {
@@ -24,8 +33,12 @@ function selecionarGola(item) {
     }else{
         cont++;
     }
+  
+    gola = "v"
 
     marcado.classList.add("selecionado");
+    verificarPedido();
+
 }
 
 function selecionarTecido(item) {
@@ -38,5 +51,52 @@ function selecionarTecido(item) {
         cont++;
     }
 
+    tecido = "algodão"
+
     marcado.classList.add("selecionado");
+    verificarPedido();
+}
+
+function verificarPedido() {
+    if(cont === 3){
+        const botao = document.querySelector(".confirmar-pedido");
+        botao.classList.add("clicavel")
+    }
+}
+
+
+// function validarImagem() {
+    
+//     urlImagem = document.querySelector(".url-imagem").value;
+
+// }
+
+// function confirmarPedido() {
+// }
+
+
+function confirmarPedido(){
+    const promessa = axis.post('https://mock-api.driven.com.br/api/v4/shirts-api/shirts',
+    {
+        model: modelo,
+        neck: gola,
+        material: tecido,
+        image: urlImagem,
+        owner: nome,
+        author: nome
+    }
+    
+    );
+
+    promessa.then(confirmada);
+    promessa.catch(naoConfirmada);
+    
+}
+
+function confirmada() {
+    alert("Encomenda Confirmada!");
+}
+
+function naoConfirmada() {
+    alert("Ops, não conseguimos processar sua encomenda");
 }
